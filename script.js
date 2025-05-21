5f9b6a1fee49b2b101413e87d3eb0971d1219b39
Oliverpe <158190764+Oliverpe@users.noreply.github.com>
2025-05-21 19:57:38 +0200

    Update script.js adding comments

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict"
 /**
+ * hämtar väderdata från OpenWeatherAPI
+ * query hämtar namnet på den staden som användaren söker realtids väderprognos
  * @param {string} query
+ * 
  * @param {HTMLElement}
  */
 
@@ -8,24 +11,28 @@
 
 
 function searchWeather(query) {
+    //API-nyckel
     const apikey = '895516bde4d1a80e8c585b40ce1c8c8b';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(query)}&appid=${apikey}&units=metric&lang=sv`;
-
+    
+    //rensar tidigare väderkort när användare vill sök efter en stad när hen har redan en gång sökt efter en stads väderprognos
     document.querySelector('#weather_card').innerHTML = '';
 
+    //Hämtar väderdata från API
     window.fetch(apiUrl).then(function(response) {
     
         if(!response.ok) {
-            
+            //Validering: om staden ej hittas, aktiveras ett fel meddelande
             throw new Error(`Kunde ej hitta staden ${query} du vill kolla vädret efter ${response.status}`);
     
         }
         return response.json();
     
     }).then(function(data){
+        //Loggar väderdata till console.
         console.log(data);
 
-
+        //Skapar väderkortet
         let weather_card = document.getElementById('weather_card');
         weather_card.classList.add('card');
 
@@ -33,16 +40,19 @@ function searchWeather(query) {
         weather_cardbody.classList.add('weather_cardbody', 'text-center');
         weather_card.appendChild(weather_cardbody);        
 
+        //Titel med stad och land tillväderkort
         let weather_titel = document.createElement('h1');
         weather_titel.classList.add('weather_title', 'mb-3');
         weather_titel.textContent = `${data.name}, ${data.sys.country}`;
         weather_cardbody.appendChild(weather_titel);
 
+        //Väderikoner till väderkort
         let weather_icon = document.createElement('img');
         weather_icon.classList.add('weather_icon', 'mb-3');
         weather_icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
         weather_cardbody.appendChild(weather_icon);
 
+        //Skapar en rad för tempratur och beskrivning till väderkort
         let row1 = document.createElement('div');
         row1.classList.add('row');
         weather_cardbody.appendChild(row1);
@@ -52,17 +62,19 @@ function searchWeather(query) {
         
 
 
-
+        // Tempratur till väderkort
         let weather_temp = document.createElement('h2');
         weather_temp.classList.add('weather_temp');
         weather_temp.textContent = `${data.main.temp}°C `;
         //weather_cardbody.appendChild(weather_temp);
 
+        //Tempratur det känns om till väderkort 
         let weather_tempfl = document.createElement('p');
         weather_tempfl.classList.add('weather_tempfl');
         weather_tempfl.textContent = `känns som ${data.main.feels_like}°C`
         weather_cardbody.appendChild(weather_tempfl);
 
+        //Väderbeskrivning till väderkort
         let weather_desc = document.createElement('p');
         weather_desc.classList.add('weather_desc');
         weather_desc.textContent = `${data.weather[0].description}`;
@@ -71,7 +83,7 @@ function searchWeather(query) {
         
 
         //container.appendChild(weather_card);
-
+        //lägger till element i kolumnen och raden
         row1.appendChild(col);
         col.append(weather_temp, weather_desc,);
     })
@@ -79,7 +91,7 @@ function searchWeather(query) {
 }
 
 
-
+//Lyssnare när användare hamnar på sidan
 window.addEventListener("load", function() {
        
     /**
@@ -122,17 +134,17 @@ window.addEventListener("load", function() {
     
        
 
-        
-
+        //Lyssnare som lyssnar på efter formulärets submit händelse 
         document.querySelector('#sokfaltform').addEventListener('submit', function(event) {
-            event.preventDefault();
+            event.preventDefault();//förhindrar att sidan laddas om
 
-            const query = sokfalt.value.trim();
+            const query = sokfalt.value.trim();//Hämtar använderens input och tar bort white space
             if(!query) {
-                
+                //Om sökfältet är tomt så aktiveras felmeddelande
                 throw new Error('var vänligen fyll i namnet på en stad du vill undersöka vädret');
         
             } else {
+                //Anropar väderfunktionen och tömmer sökfältet
                 searchWeather(query);
 
                 document.getElementById('sokfalt').value = '';
@@ -151,6 +163,9 @@ window.addEventListener("load", function() {
 /*const sokKnapp = this.document.createElement('button');
 sokKnapp.setAttribute('textcontent', "Sök");
 console.log(sokKnapp);
+
+sokForm.append(sokfalt, sokKnapp);
+});*/
 
 sokForm.append(sokfalt, sokKnapp);
 });*/
